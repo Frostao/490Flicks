bf66a32d8b27416cf29e01e8e980765d608166dd
Carl Chen <chen1725@purdue.edu>
2016-01-12 07:01:14 -0500

    install pods

---
+++ Flicks/Flicks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flicks
+//
+//  Created by Carl Chen on 1/11/16.
+//  Copyright © 2016 Zhen Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
